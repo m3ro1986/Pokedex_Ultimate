e6472d729fcac9ca2150952d3346c4015dfc8eec
m3ro1986 <m3ro_1986@outlook.com>
2023-04-23 18:19:30 -0600

    pokecards

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -2,25 +2,29 @@ import { useEffect, useState } from 'react';
 import '../styles/pokedex.css';
 import PokeCard from '../components/PokeCard';
 import axios from 'axios';
+import { useSelector } from 'react-redux';
 
 const Pokedex = () => {
+    const trainerName = useSelector( state => state.trainerName );
+    const [pokemons, setPokemons] = useState([]);
 
-    const [ pokemons, setPokemons ] = useState([]);
-
-    useEffect( () => {
-        axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=40&offset=0`)
-            .then( res => setPokemons( res.data.results ))
+    useEffect(() => {
+        axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=60&offset=0`)
+            .then(res => setPokemons(res.data.results))
     }, [])
 
     return (
         <div className='pokedexBack'>
-            <header>hello world</header>
+            <header>Welcome { trainerName }, here you can find your favorite POKEMON!</header>
+            <span></span>
             <main>
-                {
-                    pokemons.map( e => (
-                        <PokeCard key={e.url} url={e.url} />
-                    ))
-                }
+                <div className='pokemonList'>
+                    {
+                        pokemons.map(e => (
+                            <PokeCard key={e.url} url={e.url} />
+                        ))
+                    }
+                </div>
             </main>
             <footer></footer>
         </div>
